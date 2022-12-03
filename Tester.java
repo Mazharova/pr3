fe8ddcd37db93450ae6e9bb33d849a632f62098a
Lolita <66422533+Mazharova@users.noreply.github.com>
2022-12-03 16:44:31 +0300

    Add files via upload

---
+++ Tester.java
@@ -0,0 +1,12 @@
+import p2.Book;
+
+public class Tester {
+    public static void main(String[] args) {
+        Circle mircle = new Circle("blin", 12, 312);
+        System.out.println(mircle.toString());
+
+        Book muk = new Book("Prestuplenie i nakazanie", "blue", 204);
+        System.out.println(muk.toString());
+
+    }
+}
